63083fbcd5dc376ef5a2b91a2f5e216c299cbc87
cd4761 <cd4761@naver.com>
2019-07-24 16:45:30 +0900

    add setup.py

--- ecc-lib/ETH-ECC.cpp
+++ ecc-lib/ETH-ECC.cpp
@@ -90,7 +90,7 @@ eth_ecc(PyObject *self, PyObject *args){
 
 //    ethash_h256_t const* mix_hash = "0x0000000000000000000000000000000000000000000000000000000000000000";
 //    const char zerohash[32] = "0000000000000000000000000000000";
-    const char zerohash[6] = "0x000";
+    const char zerohash[5] = "0x00";
 //    int zerohash = '0x000';
 //    const char zerohash[5] = "0x00";
     ethash_h256_t const* mix_hash = (ethash_h256_t*)zerohash;
@@ -100,7 +100,7 @@ eth_ecc(PyObject *self, PyObject *args){
 
 	return Py_BuildValue("{" PY_CONST_STRING_FORMAT ":" PY_STRING_FORMAT "," PY_CONST_STRING_FORMAT ":" PY_STRING_FORMAT "}",
                          "result", nonce, 8,
-                         "mix digest", mix_hash, 6);
+                         "mix digest", mix_hash, 5);
 }
 
 
