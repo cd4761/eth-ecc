c9fec874d78c6f6a0d58a07f69dc8ab25069bf5c
cd4761 <cd4761@naver.com>
2019-07-24 16:55:37 +0900

    add setup.py

--- ecc-lib/ETH-ECC.cpp
+++ ecc-lib/ETH-ECC.cpp
@@ -100,7 +100,7 @@ eth_ecc(PyObject *self, PyObject *args){
 
 	return Py_BuildValue("{" PY_CONST_STRING_FORMAT ":" PY_STRING_FORMAT "," PY_CONST_STRING_FORMAT ":" PY_STRING_FORMAT "}",
                          "result", nonce, 8,
-                         "mix digest", mix_hash, 5);
+                         "mix digest", mix_hash, 32);
 }
 
 
